fb5f408e0d12a540aab7a324db15904828051f2c
wknowpark797 <wknowpark797@gmail.com>
2023-08-16 11:15:52 +0900

    목록 화면 출력

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import Head from 'next/head';
 import styles from './Home.module.scss';
 import clsx from 'clsx';
 import axios from 'axios';
+import Image from 'next/image';
 
 // https://www.themealdb.com 라이브러리 활용
-export default function Home(props) {
-	console.log('props: ', props);
+export default function Home({ meals }) {
+	// console.log('meals props: ', meals);
+	const newMeals = meals.slice(0, 7);
 
 	return (
 		<>
@@ -17,7 +19,30 @@ export default function Home(props) {
 			</Head>
 
 			<main className={clsx(styles.main)}>
-				<h1>Main Page</h1>
+				<figure className='visual'>
+					<article className='bg'>
+						{newMeals.map((item) => {
+							return (
+								<div key={item.idMeal} className='pic' style={{ position: 'relative', width: 370, height: 270 }}>
+									<Image
+										src={item.strMealThumb}
+										alt={item.strMeal}
+										priority
+										fill
+										quality={50}
+										style={{ objectFit: 'cover' }}
+									/>
+								</div>
+							);
+						})}
+					</article>
+
+					<article className='list'>
+						{newMeals.map((item) => {
+							return <h2 key={item.idMeal}>{item.strMeal}</h2>;
+						})}
+					</article>
+				</figure>
 			</main>
 		</>
 	);
@@ -27,11 +52,11 @@ export default function Home(props) {
 // ISR 방식 화면 확인 - 빌드 후 npm run start
 export async function getStaticProps() {
 	const { data } = await axios.get(`/filter.php?c=Seafood`);
-	console.log('Data fetching on Server: ', data);
+	// console.log('Data fetching on Server: ', data);
 
 	// props로 데이터를 넘길 때 data 안쪽의 값까지 뽑아낸 후 전달
 	return {
 		props: data,
-		revalidate: 10,
+		revalidate: 60 * 60 * 24,
 	};
 }
