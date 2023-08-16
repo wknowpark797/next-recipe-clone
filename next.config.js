b22d28bb2cb2b5324055a3a4568640e203237663
wknowpark797 <wknowpark797@gmail.com>
2023-08-16 09:49:47 +0900

    프로젝트 init 및 라이브러리 설치

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const path = require('path');
+
+const nextConfig = {
+	reactStrictMode: false,
+	sassOptions: {
+		includePath: [path.join(__dirname, 'styles')],
+		prependData: `@import "styles/_variables.scss"`,
+	},
+	images: {
+		domains: ['www.themealdb.com'],
+	},
+	compiler: { stylesComponents: true },
+};
+
+module.exports = nextConfig;
