310fcc6245074a9b8102695a1d2959e97b120114
wknowpark797 <wknowpark797@gmail.com>
2023-08-16 09:59:54 +0900

    파일 정리 및 내용 수정

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
 	reactStrictMode: false,
 	sassOptions: {
 		includePath: [path.join(__dirname, 'styles')],
-		prependData: `@import "styles/_variables.scss"`,
+		prependData: `@import "styles/_variables.scss";`,
 	},
 	images: {
 		domains: ['www.themealdb.com'],
