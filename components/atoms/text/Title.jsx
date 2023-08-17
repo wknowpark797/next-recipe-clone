e5d03fc08999025385c5c61aa508aeeb46c8a7bb
wknowpark797 <wknowpark797@gmail.com>
2023-08-17 12:17:38 +0900

    Google Font 설정 및 적용

--- components/atoms/text/Title.jsx
+++ components/atoms/text/Title.jsx
@@ -1,11 +1,33 @@
 import Link from 'next/link';
 import styles from './Title.module.scss';
 import clsx from 'clsx';
+import { Nanum_Myeongjo, Orbitron } from 'next/font/google';
+
+const nanum = Nanum_Myeongjo({
+	subsets: ['latin'],
+	weight: ['400', '700'],
+	preload: true,
+	variable: '--font-nanum', // 직접 사용할 변수명 등록, 해당 변수명을 사용하기 위해서는 클래스 등록 X
+});
+
+const orbitron = Orbitron({
+	subsets: ['latin'],
+	weight: ['400', '700'],
+	preload: true,
+	variable: '--font-orbitron',
+});
 
 function Title({ children, url, style, className }) {
 	return (
 		<h1
-			className={clsx(styles.tit, className)}
+			// 폰트 객체의 클래스명을 지정하면 안쪽의 모든 폰트는 해당 폰트가 디폴트로 적용
+			// 변수명을 활용해서 선별적으로 쓰고 싶을 때 객체.variable (해당 특정요소에만 적용, 자식요소 상속X)
+			className={clsx(
+				styles.tit,
+				className,
+				nanum.variable,
+				orbitron.variable
+			)}
 			// url 속성 유무로 자식에게 링크가 있는지 파악 후
 			// 링크가 있으면 상위요소인 h1 엘리먼트에 transition 속성 제거
 			// 링크가 없으면 transition 속성 추가
