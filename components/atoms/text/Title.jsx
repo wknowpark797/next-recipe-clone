2aece35241ce6d3763166c407290fdb3e1673aae
wknowpark797 <wknowpark797@gmail.com>
2023-08-17 11:19:07 +0900

    Title.jsx url props 속성유무에 따라 transition 분기처리

--- components/atoms/text/Title.jsx
+++ components/atoms/text/Title.jsx
@@ -4,8 +4,21 @@ import clsx from 'clsx';
 
 function Title({ children, url, style, className }) {
 	return (
-		<h1 className={clsx(styles.tit, className)} style={style}>
-			{url ? <Link href={url}>{children}</Link> : { children }}
+		<h1
+			className={clsx(styles.tit, className)}
+			// url 속성 유무로 자식에게 링크가 있는지 파악 후
+			// 링크가 있으면 상위요소인 h1 엘리먼트에 transition 속성 제거
+			// 링크가 없으면 transition 속성 추가
+			style={url ? style : { ...style, transitionDuration: '0.5s' }}
+		>
+			{url ? (
+				// 링크가 있으면 transition이 적용되어야 하기 때문에 해당 속성 추가
+				<Link href={url} style={{ transitionDuration: '0.5s' }}>
+					{children}
+				</Link>
+			) : (
+				children
+			)}
 		</h1>
 	);
 }
