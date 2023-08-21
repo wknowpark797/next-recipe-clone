95b1d467423fed4333e59679d8ba9e016a638d8c
wknowpark797 <wknowpark797@gmail.com>
2023-08-21 12:03:55 +0900

    Swiper Slide 활성화 처리

--- components/organisms/Swiper/Swiper.jsx
+++ components/organisms/Swiper/Swiper.jsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Autoplay } from 'swiper';
 import 'swiper/css';
 import Title from '@/components/atoms/text/Title';
+import { useState } from 'react';
 
 // Next에서 Autoplay, Pagination, Navigation 기능을 활성화하기 위해 SwiperCore.use 사용
 SwiperCore.use([Autoplay]);
@@ -12,6 +13,9 @@ SwiperCore.use([Autoplay]);
 function SwiperWrap({ recipe, category }) {
 	console.log('recipe: ', recipe);
 	console.log('category: ', category);
+
+	const [Index, setIndex] = useState(0);
+	console.log('index: ', Index);
 
 	return (
 		<figure className={clsx(styles.visual)}>
@@ -42,6 +46,9 @@ function SwiperWrap({ recipe, category }) {
 						spaceBetween: 50,
 					},
 				}}
+				// loop 기능 적용시 슬라이드가 동적으로 추가되기 때문에 순번의 어그러짐 발생
+				// slideChange 이벤트 발생시 자동으로 전달되는 파라미터 객체의 realIndex 프로퍼티 활용
+				onSlideChange={(el) => setIndex(el.realIndex)}
 			>
 				{recipe.map((item) => {
 					return (
@@ -49,13 +56,19 @@ function SwiperWrap({ recipe, category }) {
 							key={item.idMeal}
 							className={clsx(styles.swiperSlide)}
 						>
-							<div>
-								<Title tag={'h3'} url={'/'} type={'slogan'}>
-									{item.strMeal.length > 25
-										? item.strMeal.substr(0, 25)
-										: item.strMeal}
-								</Title>
-							</div>
+							{/* SwiperSlide 컴포넌트 안쪽에서 자동으로 JSX를 리턴하는 함수 호출 가능 */}
+							{/* 해당 함수에는 파라미터로 현재 컴포넌트 요소가 활성화되어 있는 구분할 수 있는 객체가 전달 */}
+							{({ isActive }) => {
+								return (
+									<div className={clsx(isActive ? styles.on : '')}>
+										<Title tag={'h3'} url={'/'} type={'slogan'}>
+											{item.strMeal.length > 25
+												? item.strMeal.substr(0, 25)
+												: item.strMeal}
+										</Title>
+									</div>
+								);
+							}}
 						</SwiperSlide>
 					);
 				})}
