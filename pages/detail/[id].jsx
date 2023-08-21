97e8e3b10ffdf32ada55cd93413444fa360ef4b3
wknowpark797 <wknowpark797@gmail.com>
2023-08-21 13:15:47 +0900

    Detail 상세 페이지 생성 및 적용

---
+++ pages/detail/[id].jsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+
+function Detail() {
+	const router = useRouter();
+	const { id, name } = router.query;
+
+	return (
+		<section>
+			<h1>{name}</h1>
+		</section>
+	);
+}
+
+export default Detail;
