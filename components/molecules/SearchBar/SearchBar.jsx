226efbc66e19474f305f12ee1bf30405ce2f40ed
wknowpark797 <wknowpark797@gmail.com>
2023-08-24 11:00:46 +0900

    SearchBar.jsx 컴포넌트 추가

---
+++ components/molecules/SearchBar/SearchBar.jsx
@@ -0,0 +1,21 @@
+import Input from '@/components/atoms/form/Input';
+import styles from './SearchBar.module.scss';
+import clsx from 'clsx';
+import Btn from '@/components/atoms/button/Btn';
+
+function SearchBar({
+	isBtn = true,
+	btnText = 'button',
+	inputType,
+	value,
+	onChange,
+}) {
+	return (
+		<div className={clsx(styles.searchBar)}>
+			<Input type={inputType} value={value} onChange={onChange} />
+			{isBtn && <Btn>{btnText}</Btn>}
+		</div>
+	);
+}
+
+export default SearchBar;
