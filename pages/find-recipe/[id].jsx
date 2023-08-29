9dd33c1456c16f68cc6490ccb28935599a40766a
wknowpark797 <wknowpark797@gmail.com>
2023-08-29 10:55:07 +0900

    다이나믹 라우터 페이지 트랜지션 모션 적용시 페이지를 변경할 때 prop 오류 처리 분석

--- pages/find-recipe/[id].jsx
+++ pages/find-recipe/[id].jsx
@@ -60,7 +60,15 @@ function Detail() {
 
 	return (
 		<section className={clsx(styles.detail)}>
-			{/* Spinner Loading */}
+			{/* 
+				[ Spinner Loading ]
+				Next는 라우터명이 변경될때마다 unmount되는 페이지 컴포넌트의 CSR방식으로 가져온 데이터와 스타일 노드를 제거한다.
+				page transition이 적용되어 있기 때문에 상세페이지에서 다른페이지로 넘어갈 때 데이터는 이미 사라졌음에도 불구하고
+				데이터를 활용하는 컴포넌트가 계속 있으면 prop오류 발생
+				-> 해결방법: CSR방식으로 가져오는 데이터를 컴포넌트 렌더링의 조건 설정
+					- 데이터가 없으면 로딩바 출력
+					- 데이터가 있으면 그 데이터를 활용하는 컴포넌트 출력
+			*/}
 			<RingLoader
 				cssOverride={{
 					position: 'absolute',
