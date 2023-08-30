4579309c801760f5229ec15083e43c24814b01f3
wknowpark797 <wknowpark797@gmail.com>
2023-08-30 12:25:24 +0900

    Breadcrumb.jsx 전달받은 데이터 가공 코드 개선

--- components/molecules/Breadcrumb/Breadcrumb.jsx
+++ components/molecules/Breadcrumb/Breadcrumb.jsx
@@ -7,6 +7,7 @@ function Breadcrumb({ data }) {
 	return (
 		<nav className={clsx(styles.breadcrumb)}>
 			{data.map((name, idx) => {
+				// 라우터명 문자값에 '-'있을 때 처리
 				const result = name.includes('-')
 					? name
 							.split('-')
@@ -16,12 +17,17 @@ function Breadcrumb({ data }) {
 							.join(' ')
 					: name;
 
+				// 문자값에 쿼리스트링이 있을 때 처리
+				const result2 = result.includes('?')
+					? result.split('=')[1].replaceAll('%20', ' ')
+					: result;
+
 				if (idx !== data.length - 1) {
 					// 반복되는 메뉴 순번이 마지막이 아닐 때
 					return (
 						<Fragment key={idx}>
 							<Text tag={'em'} url={`/${name}`}>
-								{name ? result : 'Home'}
+								{result ? result : 'Home'}
 							</Text>
 							<span> / </span>
 						</Fragment>
@@ -30,9 +36,7 @@ function Breadcrumb({ data }) {
 					// 마지막 순번
 					return (
 						<Text key={idx} tag={'strong'}>
-							{result.includes('?')
-								? result.split('=')[1].replaceAll('%20', ' ')
-								: result}
+							{result2}
 						</Text>
 					);
 				}
