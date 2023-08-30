55a21fa7d0ee06c57d687e3c4f56ae6723fa16a6
wknowpark797 <wknowpark797@gmail.com>
2023-08-30 12:02:18 +0900

    Breadcrumb.jsx 전달받은 라우터명 가공

--- components/molecules/Breadcrumb/Breadcrumb.jsx
+++ components/molecules/Breadcrumb/Breadcrumb.jsx
@@ -7,12 +7,21 @@ function Breadcrumb({ data }) {
 	return (
 		<nav className={clsx(styles.breadcrumb)}>
 			{data.map((name, idx) => {
+				const result = name.includes('-')
+					? name
+							.split('-')
+							.map(
+								(txt) => txt.charAt(0).toUpperCase() + txt.slice(1)
+							)
+							.join(' ')
+					: name;
+
 				if (idx !== data.length - 1) {
 					// 반복되는 메뉴 순번이 마지막이 아닐 때
 					return (
 						<Fragment key={idx}>
 							<Text tag={'em'} url={`/${name}`}>
-								{name ? name : 'home'}
+								{name ? result : 'Home'}
 							</Text>
 							<span> / </span>
 						</Fragment>
@@ -21,9 +30,9 @@ function Breadcrumb({ data }) {
 					// 마지막 순번
 					return (
 						<Text key={idx} tag={'strong'}>
-							{name.includes('?')
-								? name.split('=')[1].replaceAll('%20', ' ')
-								: name}
+							{result.includes('?')
+								? result.split('=')[1].replaceAll('%20', ' ')
+								: result}
 						</Text>
 					);
 				}
